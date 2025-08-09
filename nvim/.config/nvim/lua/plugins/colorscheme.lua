4e85de53b51e9cea61088d050efe052e3f685d69
Jagath Kiran <135314273+jagathkiran@users.noreply.github.com>
2025-08-09 12:51:12 +0530

    NeoVim config and more

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- Add midnight.nvim plugin
+  { "dasupradyumna/midnight.nvim" },
+
+  -- Configure LazyVim to load midnight
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "midnight",
+    },
+  },
+}
