dc4f32051788a206dd6c8f335b35db80f370b36a
Jagath Kiran <135314273+jagathkiran@users.noreply.github.com>
2025-09-14 16:58:34 +0530

    Change accept completion key to <Tab>

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,25 @@
+return {
+  "saghen/blink.cmp",
+  opts = function(_, opts)
+    -- Extend the default keymap with custom mappings
+    opts.keymap = vim.tbl_deep_extend("force", opts.keymap or {}, {
+      ["<CR>"] = { "fallback" }, -- Insert newline without confirming completion
+      ["<Tab>"] = {
+        "accept", -- Accept completion if menu is open
+        fallback = function()
+          vim.api.nvim_feedkeys("\t", "n", false) -- Insert literal tab character (or spaces with expandtab)
+        end,
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        "snippet_forward", -- Jump forward in snippet
+        "select_next", -- Select previous completion item if menu is open
+        fallback = function()
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false) -- Default <S-Tab> behavior
+        end,
+      },
+      ["<C-e>"] = { "cancel" }, -- Close the completion menu
+    })
+    return opts
+  end,
+}
