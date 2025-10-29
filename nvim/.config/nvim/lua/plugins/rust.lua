5ab5cbe512b82da52f6751de91931cd3f3efaaa5
Jagath Kiran <135314273+jagathkiran@users.noreply.github.com>
2025-10-30 00:27:16 +0530

    Rustaceanvim neovim plugin

---
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -0,0 +1,13 @@
+return {
+
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^6",
+    lazy = false,
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+}
