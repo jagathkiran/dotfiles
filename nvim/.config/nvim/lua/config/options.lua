58b113e4b8ddf53bbf2b28a81c71caffb3df6713
Jagath Kiran <135314273+jagathkiran@users.noreply.github.com>
2025-11-01 13:36:17 +0530

    Enable wrap by default and use 4 width tabs for nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Enable line wrapping by default
+vim.opt.wrap = true
+
+-- Use tab width of 4 spaces
+vim.opt.tabstop = 4 -- number of visual spaces per TAB
+vim.opt.shiftwidth = 4 -- number of spaces to use for autoindent
+vim.opt.softtabstop = 4 -- number of spaces per Tab when editing
+vim.opt.expandtab = false
